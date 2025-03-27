083da96b1b4513a1c49b5aa4bf05c7ba114daea6
cin <1316108264@qq.com>
2025-03-27 10:40:58 +0800

    员工分页查询业务代码开发

--- sky-server/src/main/java/com/sky/service/EmployeeService.java
+++ sky-server/src/main/java/com/sky/service/EmployeeService.java
@@ -2,9 +2,13 @@ package com.sky.service;
 
 import com.sky.dto.EmployeeDTO;
 import com.sky.dto.EmployeeLoginDTO;
+import com.sky.dto.EmployeePageQueryDTO;
 import com.sky.entity.Employee;
+import com.sky.result.PageResult;
 
 public interface EmployeeService {
+
+
 
     /**
      * 员工登录
@@ -18,4 +22,10 @@ public interface EmployeeService {
      * @param employeeDto
      */
     void save(EmployeeDTO employeeDto);
+
+    /**
+     * 员工分页查询
+     * @param employeePageQueryDTO
+     */
+     PageResult pageQuery(EmployeePageQueryDTO employeePageQueryDTO);
 }
